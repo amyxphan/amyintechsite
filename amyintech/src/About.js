e46b770f51ef5d83414722eda0858c7e49aac10c
Amy Phan <phan.amy28@gmail.com>
2024-06-20 14:03:47 -0500

    troubleshooting topnavbar

---
+++ amyintech/src/About.js
@@ -0,0 +1,13 @@
+//Projects.js 
+import React from 'react';
+import './About.css'; 
+
+function About() {
+  return (
+    <div>
+        This page will be coming soon 
+    </div>
+  );
+}
+
+export default About;
---
+++ amyintech/src/Projects.js
@@ -0,0 +1,13 @@
+//Projects.js 
+import React from 'react';
+import './Projects.css'; 
+
+function Projects() {
+  return (
+    <div>
+        This page will be coming soon 
+    </div>
+  );
+}
+
+export default Projects;
