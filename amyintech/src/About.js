9ee264d95fef76a00140a67347f398c81feb4693
Amy Phan <phan.amy28@gmail.com>
2024-06-20 17:02:02 -0500

    added some content, needs editing

--- amyintech/src/About.js
+++ amyintech/src/About.js
@@ -1,11 +1,55 @@
-//Projects.js 
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './About.css'; 
+
+// typewwriter effect for intro
+const Typewriter = ({ text, speed = 150 }) => {
+  const [displayedText, setDisplayedText] = useState('');
+  const [index, setIndex] = useState(0);
+
+  useEffect(() => {
+    if (index < text.length) {
+      const timeoutId = setTimeout(() => {
+        setDisplayedText(displayedText + text[index]);
+        setIndex(index + 1);
+      }, speed);
+      return () => clearTimeout(timeoutId);
+    }
+  }, [displayedText, index, text, speed]);
+
+  return <span>{displayedText}</span>;
+};
 
 function About() {
   return (
-    <div>
-        This about page will be coming soon 
+    <div className="About">
+      <header className="About-header">
+        <p className="About-title">
+          $ <Typewriter text="print(About Me)" />
+        </p>
+
+        <p className="About-subtitle">
+          Get to know me!
+        </p>
+
+        <p className="About-intro">
+        Hello! My name is Amy Phan and I'm a senior majoring in Computer Engineering at 
+        the University of North Texas. I'm currently a Software Engineering Intern at 
+        JPMorgan Chase & Co. and serve as a SASE South Regional Coordinator, supporting 
+        university chapters with professional development, celebrate diversity, and giving 
+        back to the community. Prior to this role, I had the honor of being the President 
+        and Co-Founder of the Society of Asian Scientists and Engineers at UNT(SASE UNT), 
+        where I played a pivotal role in establishing and leading the chapter. My interest 
+        in engineering began as a little girl. Rather than playing dress up with dolls, I 
+        loved to break and unscrew toy cars to learn how to put everything back together. As 
+        I grew older I implemented these habits into my day-to-day life. I am an aspiring 
+        software engineer, passionate about accessibility, and hope to create innovative 
+        solutions that bridge gaps and drive positive change in the tech industry.
+        </p>
+
+        <p className="About-subtitle">
+          Awards
+        </p>
+      </header>
     </div>
   );
 }
--- amyintech/src/Projects.js
+++ amyintech/src/Projects.js
@@ -1,11 +1,92 @@
 //Projects.js 
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './Projects.css'; 
+
+// typewwriter effect for intro
+const Typewriter = ({ text, speed = 150 }) => {
+  const [displayedText, setDisplayedText] = useState('');
+  const [index, setIndex] = useState(0);
+
+  useEffect(() => {
+    if (index < text.length) {
+      const timeoutId = setTimeout(() => {
+        setDisplayedText(displayedText + text[index]);
+        setIndex(index + 1);
+      }, speed);
+      return () => clearTimeout(timeoutId);
+    }
+  }, [displayedText, index, text, speed]);
+
+  return <span>{displayedText}</span>;
+};
 
 function Projects() {
   return (
-    <div>
-        This project page will be coming soon 
+    <div className="Projects">
+      <header className="Projects-header">
+        <p className="Projects-intro">
+          $ <Typewriter text="print(Projects)" />
+        </p>
+
+        <p className="Projects-subtitle">
+          TherapyTouch
+        </p>
+
+        <p className="Projects-usage">
+          using Swift and Swift UI
+        </p>
+
+        <p className="Projects-description">
+          [insert description]
+        </p>
+
+        <p className="Projects-subtitle">
+          SmartParking
+        </p>
+
+        <p className="Projects-usage">
+        used React Native JS, AWS Config, AWS DynamoDB, ESP32, and Ultrasonic Sensors
+        </p>
+
+        <p className="Projects-description">
+          [insert description]
+        </p>
+
+        <p className="Projects-subtitle">
+          Coastal Compass+
+        </p>
+
+        <p className="Projects-usage">
+          used HTML, CSS, JavaScript, Figma, Node.js, Express.js, and MongoDB
+        </p>
+
+        <p className="Projects-description">
+        This project was implemented at the 2022 CodeForGood hackathon hosted by 
+        JPMorgan Chase and Co. in October 2022. During this hackathon, I was placed 
+        randomly in a group of 6 people to help a non-profit organization expand its 
+        services to urban areas. I was able to familiarize myself with the frontend 
+        website and app development concepts and learn how to work and adapt with 
+        others who have different skill sets compared to me. In the end, our project 
+        was one of the groups that won the 2022 hackathon in Plano, TX!
+        </p>
+
+        <p className="Projects-subtitle">
+          amyintech (this website!)
+        </p>
+
+        <p className="Projects-description">
+          used HTML and CSS (recently migrating to React!) 
+        </p>
+
+        <p className="Projects-description">
+        This website is a personal project that I hope to continue to build on 
+        throughout my career. While the site is currently running in HTML and CSS,
+         I am actively planning to expand its capabilities and improve user 
+         experience. Through self-guided learning from YouTube videos and my internship, 
+         I hope to leverage my new skills to enhance the website with JavaScript and 
+         React in the near future.
+        </p>
+      </header>
     </div>
   );
 }
