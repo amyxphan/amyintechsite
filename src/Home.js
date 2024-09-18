ee4cd3548281ccbc844e6a0fd4e5eb9781673c7f
amyxphan <phan.amy28@gmail.com>
2024-09-17 20:36:14 -0500

    fixing configs for vercel deployment

--- src/Home.js
+++ src/Home.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import './Home.css';
 
 import pfp from '/Users/amyphan/Desktop/amyintechsite/src/img/pfp.jpg';
-import AmyPhanResumeJune2024 from '/Users/amyphan/Desktop/amyintechsite/public/AmyPhanResumeJune2024.pdf'
+import AmyPhanResumeJune2024 from '/Users/amyphan/Desktop/amyintechsite/src/AmyPhanResumeJune2024.pdf'
 
 // typewwriter effect for intro
 const Typewriter = ({ text, speed = 150 }) => {
@@ -51,7 +51,7 @@ function Home() {
             </a>
             <a
               className="Home-link"
-              href="/AmyPhanResumeJune2024.pdf"
+              href={AmyPhanResumeJune2024}
               target="_blank"
               rel="noopener noreferrer"
             >
