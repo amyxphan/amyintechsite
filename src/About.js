7043945c3f1158002f312ae63216194650e98eae
amyxphan <phan.amy28@gmail.com>
2024-10-27 20:05:55 -0500

    edited about captions

--- src/About.js
+++ src/About.js
@@ -153,7 +153,7 @@ function About() {
         ranked varsity girl’s basketball team. I attended the University of North Texas, earning a 
         B.S. in Computer Engineering. During my time there, I founded the Society of Asian Scientists
          and Engineers (SASE) and spent my free time learning computer science concepts beyond my 
-         formal studies. After graduation, I returned to JPMorgan Chase & Co. as a full-time Software Engineer. 
+         formal studies. I'm currently entering my new grad journey as a Software Engineer at JPMorgan Chase & Co. 
          In my free time, I enjoy hiking, pilates, playing with my pet hamster Mochi, and taking care of my 
          plants. I am passionate about accessibility and aim to create innovative solutions that 
          bridge gaps and drive positive change in the tech industry.
